5ff56fb52986f112838168844db6c21811036915
panizhalvachi <59363733+panizhalvachi@users.noreply.github.com>
2021-10-17 21:15:43 +0330

    Update Untitled.py

--- Untitled.py
+++ Untitled.py
@@ -1,20 +1,25 @@
 from scanner import get_next_token, get_line_num
 
 symbol_table_file = open("symbol_table.txt", "w")
+errors_file = open("lexical_errors.txt", "w")
 symbol_set = set()
 
 answer = dict()
 while True:
-
-    res = get_next_token()
-    
-    if res[0]=="ID":
-    if res[1] not in symbol_set:
-        symbol_set.add(res[1])
-        symbol_table_file.write(res[1]+"\n")
+    try:
+        res = get_next_token()
+    except Exception as error_msg:
+        errors_file.write(error_msg+"\n")
     
     if res is None:
         break
+        
+    if res[0]=="ID":
+        if res[1] not in symbol_set:
+            symbol_set.add(res[1])
+            symbol_table_file.write(res[1]+"\n")
+            
+
     if res[0] in ("COMMENT", "WHITESPACE"):
         continue
 
@@ -28,4 +33,6 @@ for key in answer:
     for (tp, lexeme) in answer[key]:
         print("({}, {}) ".format(tp, lexeme), end='')
     print()
+
 symbol_table_file.close()
+errors_file.close()
