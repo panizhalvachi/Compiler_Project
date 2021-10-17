9cef39cd6d1f23fda8b152b1d8d4d6f06222a225
panizhalvachi <59363733+panizhalvachi@users.noreply.github.com>
2021-10-17 20:18:21 +0330

    Symbol Table

--- Untitled.py
+++ Untitled.py
@@ -1,10 +1,18 @@
 from scanner import get_next_token, get_line_num
 
+symbol_table_file = open("symbol_table.txt", "w")
+symbol_set = set()
 
 answer = dict()
 while True:
 
     res = get_next_token()
+    
+    if res[0]=="ID":
+    if res[1] not in symbol_set:
+        symbol_set.add(res[1])
+        symbol_table_file.write(res[1]+"\n")
+    
     if res is None:
         break
     if res[0] in ("COMMENT", "WHITESPACE"):
@@ -20,3 +28,4 @@ for key in answer:
     for (tp, lexeme) in answer[key]:
         print("({}, {}) ".format(tp, lexeme), end='')
     print()
+symbol_table_file.close()
